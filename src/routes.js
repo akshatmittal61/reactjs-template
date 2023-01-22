b58495422af7a5b5f47f11265640af28a07da458
Akshat Mittal <akshatmittal2506@gmail.com>
2023-01-23 01:13:58 +0530

    styles and routes modification

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,26 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
 
+const protectedRoutes = [
+	{
+		path: "/dashboard",
+		component: <Dashboard />,
+	},
+];
+
 const routes = [
+	...protectedRoutes.map((route) => ({
+		path: route.path,
+		component: (
+			<PrivateRoute>
+				{route.component}
+			</PrivateRoute>
+		)
+	})
+	),
 	{
 		path: "/",
 		component: <Home />,
-	},
-	{
-		path: "/dashboard",
-		component: (
-			<PrivateRoute>
-				<Dashboard />
-			</PrivateRoute>
-		),
 	},
 	{
 		path: "*",
